635be3ecd51d68a29e466918c6d6fb21f7b3b292
andreshkivVC <109181911+andreshkivVC@users.noreply.github.com>
2022-12-08 01:28:23 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtn.removeAttribute('disabled', 'true');
+startBtn.setAttribute('disabled', 'true');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+stopBtn.setAttribute('disabled', 'true');
+startBtn.removeAttribute('disabled', 'true');
+});
